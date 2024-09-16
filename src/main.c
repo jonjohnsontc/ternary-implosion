d7b19baef66c79dbddc2f9350dd216175a7c303d
Jon Johnson <jonjohnsontc@live.com>
2024-09-15 19:26:45 -0700

    checkpoint

--- src/main.c
+++ src/main.c
@@ -12,9 +12,10 @@
 /* exec sql whenever sql_warning  sqlprint ; */
 #line 4 "src/main.pgc"
  
-/* exec sql whenever sqlerror  call print_sqlca ( ) ; */
+/* exec sql whenever sqlerror  sqlprint ; */
 #line 5 "src/main.pgc"
 
+// EXEC SQL WHENEVER SQLERROR CALL print_sqlca();
 
 void print_sqlca() {
     fprintf(stderr, "==== sqlca ====\n");
@@ -33,15 +34,19 @@ void print_sqlca() {
 int main() {
   /* exec sql begin declare section */
         
+        
        
   
-#line 23 "src/main.pgc"
+#line 24 "src/main.pgc"
  const char * pword = getenv ( "POSTGRES_PASSWORD" ) ;
  
-#line 24 "src/main.pgc"
+#line 25 "src/main.pgc"
+ const char * dbname = "unix:postgresql://localhost:5432/ecpg_demo" ;
+ 
+#line 26 "src/main.pgc"
  long long count ;
 /* exec sql end declare section */
-#line 25 "src/main.pgc"
+#line 27 "src/main.pgc"
   
 
   if (pword == NULL) {
@@ -49,45 +54,45 @@ int main() {
     exit(1);
   }
 
-  { ECPGconnect(__LINE__, 0, "ecpg_demo@localhost" , "jon" , pword , NULL, 0); 
-#line 32 "src/main.pgc"
+  { ECPGconnect(__LINE__, 0, dbname , NULL, NULL , NULL, 0); 
+#line 34 "src/main.pgc"
 
 if (sqlca.sqlwarn[0] == 'W') sqlprint();
-#line 32 "src/main.pgc"
+#line 34 "src/main.pgc"
 
-if (sqlca.sqlcode < 0) print_sqlca ( );}
-#line 32 "src/main.pgc"
-
+if (sqlca.sqlcode < 0) sqlprint();}
+#line 34 "src/main.pgc"
+ 
   printf("SQL connection executed\n");
   { ECPGdo(__LINE__, 0, 1, NULL, 0, ECPGst_normal, "select count ( * ) from electricity_market", ECPGt_EOIT, 
 	ECPGt_long_long,&(count),(long)1,(long)1,sizeof(long long), 
 	ECPGt_NO_INDICATOR, NULL , 0L, 0L, 0L, ECPGt_EORT);
-#line 34 "src/main.pgc"
+#line 36 "src/main.pgc"
 
 if (sqlca.sqlwarn[0] == 'W') sqlprint();
-#line 34 "src/main.pgc"
+#line 36 "src/main.pgc"
 
-if (sqlca.sqlcode < 0) print_sqlca ( );}
-#line 34 "src/main.pgc"
+if (sqlca.sqlcode < 0) sqlprint();}
+#line 36 "src/main.pgc"
 
   { ECPGtrans(__LINE__, NULL, "commit");
-#line 35 "src/main.pgc"
+#line 37 "src/main.pgc"
 
 if (sqlca.sqlwarn[0] == 'W') sqlprint();
-#line 35 "src/main.pgc"
+#line 37 "src/main.pgc"
 
-if (sqlca.sqlcode < 0) print_sqlca ( );}
-#line 35 "src/main.pgc"
+if (sqlca.sqlcode < 0) sqlprint();}
+#line 37 "src/main.pgc"
 
   printf("Total number of rows in electricity_market: %d\n", count);
   { ECPGdisconnect(__LINE__, "ALL");
-#line 37 "src/main.pgc"
+#line 39 "src/main.pgc"
 
 if (sqlca.sqlwarn[0] == 'W') sqlprint();
-#line 37 "src/main.pgc"
+#line 39 "src/main.pgc"
 
-if (sqlca.sqlcode < 0) print_sqlca ( );}
-#line 37 "src/main.pgc"
+if (sqlca.sqlcode < 0) sqlprint();}
+#line 39 "src/main.pgc"
 
   printf("SQL connected exited\n");
   return 0;
