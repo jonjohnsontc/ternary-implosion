53e7f268c80147c0c66ed86e98a4a87f59282990
Jon Johnson <jonjohnsontc@live.com>
2024-09-19 17:00:40 -0700

    adding small build script

---
+++ build.sh
@@ -0,0 +1,8 @@
+set -eux;
+# Build ecpg example
+cc -c -I/usr/local/pgsql/include src/main.c
+cc -o main main.o -L/usr/local/pgql/lib -lecpg -lpgtypes
+
+# Build lipbq example
+cc -c -I/usr/local/pgsql/include src/libpq_example.c;
+cc -o libpq_ex libpq_example.o -L/usr/local/pgql/lib -lpq;
